bd25bdce4eac3d3e51e6f7a995ffbdc5f69243a1
Ali Maldondo <alimaldonado.1995@gmail.com>
2020-06-03 20:06:45 -0700

    created tables for products and packagings

---
+++ products.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE zapatos_rotos;
+
+USE zapatos_rotos;
+
+-- id type uuid
+CREATE TABLE tbl_products (
+    id CHAR(36) NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    -- aka value
+    price DECIMAL(8,2) NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL
+) DEFAULT CHARACTER SET utf8mb4
+            COLLATE utf8mb4_general_ci
+            engine = innodb;
+
+CREATE TABLE tbl_packagings (
+    id CHAR(36) NOT NULL PRIMARY KEY,
+    name VARCHAR (100) NOT NULL,
+    type VARCHAR (100) NOT NULL,
+    code VARCHAR (5) NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL
+) DEFAULT CHARACTER SET utf8mb4
+            COLLATE utf8mb4_general_ci
+            engine = innodb;
+/*
+    pivot table allows one product to have
+    several packagings  
+*/
+CREATE TABLE tbl_products_packagings (
+    product_id CHAR(36) NOT NULL,
+    packaging_id CHAR(36) NOT NULL,
+    -- unique prevents repeating registries
+    UNIQUE(product_id, packaging_id),
+    CONSTRAINT fk_packaging_product_id FOREIGN KEY(product_id)
+    REFERENCES tbl_products(id) ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT fk_product_packaging_id FOREIGN KEY(packaging_id)
+    REFERENCES tbl_packagings(id) ON UPDATE CASCADE ON DELETE RESTRICT
+) DEFAULT CHARACTER SET utf8mb4
+            COLLATE utf8mb4_general_ci
+            engine = innodb;
+
